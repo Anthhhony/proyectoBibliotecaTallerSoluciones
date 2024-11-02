5bd53e66b83948aec830b33d4ab7dc4bf22c0b56
Alejandro González <andresg.vergara04@gmail.com>
2024-11-02 00:03:20 -0300

    prestamo | prestamos
    Se desarrolló todo el proceso de la base de datos y se hicieron las funcionalidades básicas para agregar préstamos, y visualizarlos

--- proyectoBiblioteca/AppBiblioteca/admin.py
+++ proyectoBiblioteca/AppBiblioteca/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from AppBiblioteca.models import Libro, Categoria, Cliente, Prestamo, Usuario
 
 # Register your models here.
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('isbn','titulo', 'autor', 'editorial', 'anio_publicacion', 'disponibilidad')
+    list_filter = ('disponibilidad',)
+    search_fields = ('titulo', 'isbn')
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion', 'telefono', 'correo', 'usuario')
+    search_fields = ('nombre', 'correo')
