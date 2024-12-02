f1a63619c717f19c2431e2e1d2c398b885e4be31
Anthhhony <maldonadocampusanoanthy@gmail.com>
2024-12-01 23:11:24 -0300

    ...

--- proyectoBiblioteca/AppBiblioteca/apps.py
+++ proyectoBiblioteca/AppBiblioteca/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class AppbibliotecaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'AppBiblioteca'
+
+# Asegúrate de importar las señales
--- proyectoBiblioteca/proyectoBiblioteca/settings.py
+++ proyectoBiblioteca/proyectoBiblioteca/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'proyectoBiblioteca.wsgi.app'
+WSGI_APPLICATION = 'proyectoBiblioteca.wsgi.application'
 
 
 # Database
